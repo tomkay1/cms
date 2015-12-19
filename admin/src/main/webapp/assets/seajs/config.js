0785935c79abf8b2d24b4c2b0327db9de6fe0351
a312586670 <supper@9126.org>
2015-12-19 16:05:56 +0800

    改变webapp中资源目录结构

---
+++ admin/src/main/webapp/assets/seajs/config.js
@@ -0,0 +1,9 @@
+seajs.config({
+	alias: {
+		"jquery": "js/jquery-1.9.1.min.js",
+		"bootstrap": "js/bootstrap.min.js",
+		"validate": "libs/validate/jquery.validate.min.js",
+		"message": "libs/validate/jquery.validate.addMethod.js",
+	},	
+	preload: ['jquery']
+});

93a3d2facc3a57a3ea6876afca0563cdb49da627
cwb <815498030@qq.com>
2015-12-30 13:59:21 +0800

    ManyToMany Test Case

--- service/src/test/java/com/huotu/hotcms/service/SiteHostServiceTest.java
+++ service/src/test/java/com/huotu/hotcms/service/SiteHostServiceTest.java
@@ -14,6 +14,7 @@ import com.huotu.hotcms.entity.Site;
 import com.huotu.hotcms.repository.HostRepository;
 import com.huotu.hotcms.repository.SiteRepository;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,8 +25,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.EntityManagerFactory;
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 /**
  * Created by cwb on 2015/12/30.
@@ -37,11 +38,33 @@ import java.time.LocalDateTime;
 @Transactional
 public class SiteHostServiceTest {
 
-    @Autowired
-    private HostRepository hostRepository;
+    private Host host1;
+    private Host host2;
+    private String domain1;
+    private String domain2;
 
     @Autowired
+    private HostRepository hostRepository;
+    @Autowired
     private SiteRepository siteRepository;
+
+    @Before
+    public void setUp() {
+        host1 = new Host();
+        domain1 = UUID.randomUUID().toString();
+        while (hostRepository.findByDomain(domain1)!=null) {
+            domain1 = UUID.randomUUID().toString();
+        }
+        host1.setDomain(domain1);
+        host1.setCustomerId(3447);
+        host2 = new Host();
+        domain2 = UUID.randomUUID().toString();
+        while (hostRepository.findByDomain(domain2)!=null) {
+            domain2 = UUID.randomUUID().toString();
+        }
+        host2.setDomain(domain2);
+        host2.setCustomerId(3447);
+    }
 
     /**
      * 测试多对多关系建立正确性
@@ -51,27 +74,21 @@ public class SiteHostServiceTest {
     @Rollback
     public void addHostTest() {
         //新建站点新绑定域名，级联保存并建立关系
-        Host host = new Host();
-        host.setDomain("cms.51flashmall.com");
-        host.setCustomerId(3447);
-        Host host1 = new Host();
-        host1.setDomain("localhost");
-        host1.setCustomerId(3447);
         Site site = new Site();
         site.setCustomerId(3447);
         site.setTitle("火图科技");
         site.setDescription("杭州火图科技有限公司是一家专业的微信商城服务提供商，" +
                 "专业/质优/高端微商城定制开发，为客户建立专属微信三级分销系统，并提供代运营、微商人才培训等服务");
         site.setCreateTime(LocalDateTime.now());
-        site.addHost(host);
         site.addHost(host1);
+        site.addHost(host2);
         site = siteRepository.saveAndFlush(site);
         site = siteRepository.findOne(site.getSiteId());
-        Assert.assertEquals(site.getHosts().size(),2);
-        host = hostRepository.findByDomain("cms.51flashmall.com");
-        host1 = hostRepository.findByDomain("localhost");
-        Assert.assertEquals(1,host.getSites().size());
+        Assert.assertEquals(2,site.getHosts().size());
+        host1 = hostRepository.findByDomain(domain1);
+        host2 = hostRepository.findByDomain(domain2);
         Assert.assertEquals(1,host1.getSites().size());
+        Assert.assertEquals(1, host2.getSites().size());
 
         //新建站点绑定旧域名，建立关系
         Site s = new Site();
@@ -81,28 +98,28 @@ public class SiteHostServiceTest {
                 "professional / quality / high-end micro mall custom development……");
         s.setCreateTime(LocalDateTime.now());
         s = siteRepository.saveAndFlush(s);
-        s.addHost(hostRepository.findByDomain("cms.51flashmall.com"));
+        s.addHost(hostRepository.findByDomain(domain1));
         s = siteRepository.saveAndFlush(s);
         s = siteRepository.findOne(s.getSiteId());
         Assert.assertEquals(s.getHosts().size(),1);
 
         //站点解除其中一个域名绑定关系
-        site.removeHost(host);
+        site.removeHost(host1);
         site = siteRepository.saveAndFlush(site);
         Assert.assertEquals(1,site.getHosts().size());
-        host = hostRepository.findByDomain("cms.51flashmall.com");
-        Assert.assertEquals(1,host.getSites().size());
+        host1 = hostRepository.findByDomain(domain1);
+        Assert.assertEquals(1,host1.getSites().size());
 
         //域名解除其中一个站点
-        host.removeSite(s);
-        hostRepository.saveAndFlush(host);
+        host1.removeSite(s);
+        hostRepository.saveAndFlush(host1);
         Assert.assertEquals(0,s.getHosts().size());
 
         //域名绑定一个站点
-        host1.addSite(s);
-        hostRepository.saveAndFlush(host1);
+        host2.addSite(s);
+        hostRepository.saveAndFlush(host2);
         Assert.assertEquals(1,s.getHosts().size());
-        Assert.assertEquals("localhost",s.getHosts().iterator().next().getDomain());
+        Assert.assertEquals(domain2,s.getHosts().iterator().next().getDomain());
     }
 
 }

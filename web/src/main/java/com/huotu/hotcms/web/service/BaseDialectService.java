4272b0dbba07b1306ae058cef5eca4e929cc46e4
a312586670 <supper@9126.org>
2015-12-31 11:57:14 +0800

    add thymeleaf foreach dialect

--- web/src/main/java/com/huotu/hotcms/web/service/BaseDialectService.java
+++ web/src/main/java/com/huotu/hotcms/web/service/BaseDialectService.java
@@ -3,7 +3,13 @@ package com.huotu.hotcms.web.service;
 import org.thymeleaf.model.IProcessableElementTag;
 
 /**
- * Created by Administrator on 2015/12/31.
+ * <P>
+ *    Thymeleaf 自定义方言循环数据解析基类
+ * </P>
+ * @since 1.0.0
+ *
+ * @author xhl
+ *
  */
 public abstract class BaseDialectService {
    public Object  resolveDataByAttr(IProcessableElementTag elementTag) {

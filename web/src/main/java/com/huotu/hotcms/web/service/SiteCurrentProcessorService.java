a63aef9d4fdab741ac73242b45178667b6675102
a312586670 <supper@9126.org>
2016-01-06 09:19:28 +0800

    add SiteResolverService

---
+++ web/src/main/java/com/huotu/hotcms/web/service/SiteCurrentProcessorService.java
@@ -0,0 +1,38 @@
+package com.huotu.hotcms.web.service;
+
+import com.huotu.hotcms.service.entity.Site;
+import com.huotu.hotcms.web.util.PatternMatchUtil;
+import com.huotu.hotcms.web.util.StringUtil;
+import org.springframework.web.context.ContextLoader;
+import org.springframework.web.context.WebApplicationContext;
+import org.thymeleaf.context.ITemplateContext;
+import org.thymeleaf.expression.IExpressionObjects;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by Administrator on 2016/1/6.
+ */
+public class SiteCurrentProcessorService extends BaseProcessorService{
+    private static final String regexp="\\$\\{([^\\}]+)}";//匹配${key}模式的正则表达式
+
+    @Override
+    public Object resolveDataByAttr(String attributeValue, ITemplateContext context){
+        IExpressionObjects expressContent= context.getExpressionObjects();
+        HttpServletRequest request=(HttpServletRequest)expressContent.getObject("request");
+        WebApplicationContext applicationContext = ContextLoader.getCurrentWebApplicationContext();
+        SiteResolveService siteResolveService = (SiteResolveService)applicationContext.getBean("siteResolveService");
+        try {
+            Site site = siteResolveService.getHomeSite(request);
+            String attributeName= PatternMatchUtil.getMatchVal(attributeValue, regexp);
+            attributeName= StringUtil.toUpperCase(attributeName);
+            Object object = site.getClass().getDeclaredMethod("get"+attributeName).invoke(site);
+            return object;
+        }
+        catch (Exception ex)
+        {
+            //写错误日志操作
+        }
+        return null;
+    }
+}

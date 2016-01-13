794ffe1107e5a0d1dc0bbb2da11e8ec353d04564
a312586670 <supper@9126.org>
2016-01-13 15:39:01 +0800

    新增过滤器解决静态资源问题

--- web/src/main/java/com/huotu/hotcms/web/controller/IndexController.java
+++ web/src/main/java/com/huotu/hotcms/web/controller/IndexController.java
@@ -10,12 +10,9 @@ package com.huotu.hotcms.web.controller;
 
 
 import com.huotu.hotcms.service.service.SiteService;
-import com.sun.javafx.sg.prism.NGShape;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;

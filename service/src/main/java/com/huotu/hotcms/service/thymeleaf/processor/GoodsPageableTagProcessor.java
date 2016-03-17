a8d38f4826b5a2e15b983f79bdc1d1f18f321384
cwb <815498030@qq.com>
2016-03-17 20:31:10 +0800

    增加商品分页组件处理器

---
+++ service/src/main/java/com/huotu/hotcms/service/thymeleaf/processor/GoodsPageableTagProcessor.java
@@ -0,0 +1,63 @@
+/*
+ * 版权所有:杭州火图科技有限公司
+ * 地址:浙江省杭州市滨江区西兴街道阡陌路智慧E谷B幢4楼
+ *
+ *  (c) Copyright Hangzhou Hot Technology Co., Ltd.
+ *  Floor 4,Block B,Wisdom E Valley,Qianmo Road,Binjiang District 2013-2015. All rights reserved.
+ */
+
+package com.huotu.hotcms.service.thymeleaf.processor;
+
+import com.huotu.hotcms.service.entity.Site;
+import com.huotu.hotcms.service.model.thymeleaf.foreach.PageableForeachParam;
+import com.huotu.hotcms.service.thymeleaf.expression.DialectAttributeFactory;
+import com.huotu.hotcms.service.thymeleaf.expression.VariableExpression;
+import com.huotu.hotcms.service.widget.model.Goods;
+import com.huotu.hotcms.service.widget.model.GoodsSearcher;
+import com.huotu.hotcms.service.widget.model.Page;
+import com.huotu.hotcms.service.widget.service.GoodsService;
+import org.springframework.web.context.ContextLoader;
+import org.springframework.web.context.WebApplicationContext;
+import org.thymeleaf.context.ITemplateContext;
+import org.thymeleaf.dialect.IProcessorDialect;
+import org.thymeleaf.engine.AttributeName;
+import org.thymeleaf.model.IProcessableElementTag;
+import org.thymeleaf.processor.element.AbstractAttributeTagProcessor;
+import org.thymeleaf.processor.element.IElementTagStructureHandler;
+import org.thymeleaf.templatemode.TemplateMode;
+
+/**
+ * 商品分页组件处理器
+ * Created by cwb on 2016/3/17.
+ */
+public class GoodsPageableTagProcessor extends AbstractAttributeTagProcessor {
+    public static final String ATTR_NAME = "page";
+    public static final int PRECEDENCE = 1300;
+
+    public GoodsPageableTagProcessor(IProcessorDialect dialect, String dialectPrefix) {
+        super(dialect, TemplateMode.HTML, dialectPrefix, null, false, ATTR_NAME, true, PRECEDENCE, true);
+    }
+
+    @Override
+    protected void doProcess(ITemplateContext context, IProcessableElementTag tag, AttributeName attributeName, String attributeValue, String attributeTemplateName, int attributeLine, int attributeCol, IElementTagStructureHandler structureHandler) {
+        final Object iteratedValue;
+        iteratedValue = invokeGoodsPageableService(tag, context);
+        structureHandler.iterateElement(attributeValue, null, iteratedValue);
+    }
+
+    private Object invokeGoodsPageableService(IProcessableElementTag tag, ITemplateContext context) {
+        WebApplicationContext applicationContext = ContextLoader.getCurrentWebApplicationContext();
+        GoodsService goodsService = (GoodsService)applicationContext.getBean("goodsServiceImpl");
+        Site site = (Site) VariableExpression.getVariable(context, "site");
+        int customerId = site.getCustomerId();
+        Page<Goods> goodsPage = null;
+        try {
+            GoodsSearcher goodsSearcher = DialectAttributeFactory.getInstance().getForeachParam(tag, GoodsSearcher.class);
+
+        goodsPage = goodsService.searchGoods(customerId,goodsSearcher);
+        }catch (Exception e) {
+
+        }
+        return goodsPage.getRecords();
+    }
+}

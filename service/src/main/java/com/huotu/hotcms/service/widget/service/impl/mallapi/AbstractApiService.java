b342b6339574ee281d7420694305e0feaf1dce0c
a312586670 <supper@9126.org>
2016-03-23 20:54:15 +0800

    1、图片库整理 2、编辑界面整理

---
+++ service/src/main/java/com/huotu/hotcms/service/widget/service/impl/mallapi/AbstractApiService.java
@@ -0,0 +1,53 @@
+package com.huotu.hotcms.service.widget.service.impl.mallapi;
+
+import com.huotu.hotcms.service.util.ApiResult;
+import com.huotu.hotcms.service.util.HttpUtils;
+import com.huotu.hotcms.service.widget.service.MallApiEnvironmentService;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.util.StringUtils;
+
+import java.util.Map;
+
+/**
+ * Created by Administrator on 2016/3/23.
+ */
+public  abstract class AbstractApiService implements MallApiEnvironmentService {
+    private static final Log log = LogFactory.getLog(AbstractApiService.class);
+
+    protected  String serviceHost;
+
+    public String scheme;
+
+    @Override
+    public ApiResult<String> HttpGet(String path,Map<String, Object> params) {
+        try {
+            String url="";
+            if(StringUtils.isEmpty(this.scheme)){
+                url="http://"+this.serviceHost+path;
+            }else{
+                url=this.scheme+"://"+this.serviceHost+path;
+            }
+            return HttpUtils.httpGet(url,params);
+        } catch (Exception e) {
+            log.error("请求接口失败", e);
+            throw new InternalError("请求接口失败");
+        }
+    }
+
+    @Override
+    public ApiResult<String> HttpPost(String path, Map<String, Object> params) {
+        try {
+            String url="";
+            if(StringUtils.isEmpty(this.scheme)){
+                url="http://"+this.serviceHost+path;
+            }else{
+                url=this.scheme+"://"+this.serviceHost+path;
+            }
+            return HttpUtils.httpPost(url,params);
+        } catch (Exception e) {
+            log.error("请求接口失败", e);
+            throw new InternalError("请求接口失败");
+        }
+    }
+}
---
+++ service/src/main/java/com/huotu/hotcms/service/widget/service/impl/mallapi/LocalApiService.java
@@ -0,0 +1,25 @@
+package com.huotu.hotcms.service.widget.service.impl.mallapi;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.WebApplicationContext;
+
+
+/**
+ * 本地测试的对接MallApi接口
+ * @author xhl
+ */
+@Service
+@Profile("!container")
+public class LocalApiService extends  AbstractApiService  {
+    private static final Log log = LogFactory.getLog(DynamicApiService.class);
+
+    @Autowired
+    public void setWebApplicationContext(WebApplicationContext context){
+        this.serviceHost="devmallapi.huobanj.cn/";
+        this.scheme="http";
+    }
+}

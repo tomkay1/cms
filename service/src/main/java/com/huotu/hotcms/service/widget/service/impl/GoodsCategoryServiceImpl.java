04bbe8865db632f6cdd7164bd3e42d09382a0938
a312586670 <supper@9126.org>
2016-04-19 17:33:18 +0800

    1，编辑时去除链接

--- service/src/main/java/com/huotu/hotcms/service/widget/service/impl/GoodsCategoryServiceImpl.java
+++ service/src/main/java/com/huotu/hotcms/service/widget/service/impl/GoodsCategoryServiceImpl.java
@@ -8,7 +8,6 @@ import com.huotu.hotcms.service.widget.service.GoodsCategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;

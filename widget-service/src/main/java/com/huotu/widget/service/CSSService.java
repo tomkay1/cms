f3d6692ae8f80efeac204266ca690e64e3d30923
FawziKiKi <507607337@qq.com>
2016-05-31 17:42:31 +0800

    单元测试

---
+++ widget-service/src/main/java/com/huotu/widget/service/CSSService.java
@@ -0,0 +1,16 @@
+package com.huotu.widget.service;
+
+/**
+ * Created by wenqi on 2016/5/31.
+ */
+
+/**
+ * css相关服务
+ */
+public interface CSSService {
+    /**
+     * 将less文件编译成css文件
+     */
+    void convertLess2Css();
+
+}

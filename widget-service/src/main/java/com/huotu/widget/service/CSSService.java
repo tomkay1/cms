363e2a58b2afbf072be18885ca47c4d321636150
FawziKiKi <507607337@qq.com>
2016-05-31 18:00:13 +0800

    css

--- widget-service/src/main/java/com/huotu/widget/service/CSSService.java
+++ widget-service/src/main/java/com/huotu/widget/service/CSSService.java
@@ -10,7 +10,9 @@ package com.huotu.widget.service;
 public interface CSSService {
     /**
      * 将less文件编译成css文件
+     * @param cssName 输出css文件路劲 全路劲
+     * @param fileName less文件 全路劲
      */
-    void convertLess2Css();
+    void convertLess2Css(String fileName,String cssName);
 
 }

cf9df348dfa861fb54f07a5f9fbd8e484ab71406
HZCJ <luffy.ja@gmail.com>
2016-06-01 21:49:15 +0800

    add skeleton.

---
+++ widget-skeleton/src/main/resources/archetype-resources/src/main/java/WidgetInfo.java
@@ -0,0 +1,19 @@
+/*
+ * 版权所有:杭州火图科技有限公司
+ * 地址:浙江省杭州市滨江区西兴街道阡陌路智慧E谷B幢4楼
+ *
+ * (c) Copyright Hangzhou Hot Technology Co., Ltd.
+ * Floor 4,Block B,Wisdom E Valley,Qianmo Road,Binjiang District
+ * 2013-2016. All rights reserved.
+ */
+
+package ${package};
+
+import com.huotu.hotcms.widget.Widget;
+
+/**
+ * @author CJ
+ */
+public class WidgetInfo implements Widget{
+
+}

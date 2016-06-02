71d833585d7628dd1122d410390f960ca1501ec4
HZCJ <luffy.ja@gmail.com>
2016-06-02 10:50:07 +0800

    comment it.

--- widget-service/src/main/java/com/huotu/hotcms/widget/service/CSSService.java
+++ widget-service/src/main/java/com/huotu/hotcms/widget/service/CSSService.java
@@ -18,13 +18,13 @@ import java.io.OutputStream;
  * css相关服务
  */
 public interface CSSService {
-    /**
-     * 将less文件编译成css文件
-     *
-     * @param cssName  输出css文件路劲 全路劲
-     * @param fileName less文件 全路劲
-     */
-    void convertLess2Css(String fileName, String cssName);
+//    /**
+//     * 将less文件编译成css文件
+//     *
+//     * @param cssName  输出css文件路劲 全路劲
+//     * @param fileName less文件 全路劲
+//     */
+//    void convertLess2Css(String fileName, String cssName);
 
     /**
      * 将页面主题转换为既定规格的css

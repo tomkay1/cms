3c00435c0ec8929d04b4774cb14d2e6282339469
HZCJ <luffy.ja@gmail.com>
2016-06-02 10:03:42 +0800

    css service.

---
+++ widget-service/src/main/java/com/huotu/hotcms/widget/PageTheme.java
@@ -0,0 +1,37 @@
+/*
+ * 版权所有:杭州火图科技有限公司
+ * 地址:浙江省杭州市滨江区西兴街道阡陌路智慧E谷B幢4楼
+ *
+ * (c) Copyright Hangzhou Hot Technology Co., Ltd.
+ * Floor 4,Block B,Wisdom E Valley,Qianmo Road,Binjiang District
+ * 2013-2016. All rights reserved.
+ */
+
+package com.huotu.hotcms.widget;
+
+import org.springframework.core.io.Resource;
+
+/**
+ * 页面风格
+ *
+ * @author CJ
+ */
+public interface PageTheme {
+
+    /**
+     * 主色调
+     *
+     * @return 可以为null
+     */
+    String mainColor();
+
+    // todo more styles
+
+    /**
+     * 高级用户自定义的LESS
+     *
+     * @return LESS资源
+     */
+    Resource customLess();
+
+}
--- widget-service/src/main/java/com/huotu/hotcms/widget/service/CSSService.java
+++ widget-service/src/main/java/com/huotu/hotcms/widget/service/CSSService.java
@@ -1,8 +1,18 @@
+/*
+ * 版权所有:杭州火图科技有限公司
+ * 地址:浙江省杭州市滨江区西兴街道阡陌路智慧E谷B幢4楼
+ *
+ * (c) Copyright Hangzhou Hot Technology Co., Ltd.
+ * Floor 4,Block B,Wisdom E Valley,Qianmo Road,Binjiang District
+ * 2013-2016. All rights reserved.
+ */
+
 package com.huotu.hotcms.widget.service;
 
-/**
- * Created by wenqi on 2016/5/31.
- */
+import com.huotu.hotcms.widget.PageTheme;
+
+import java.io.IOException;
+import java.io.OutputStream;
 
 /**
  * css相关服务
@@ -10,9 +20,20 @@ package com.huotu.hotcms.widget.service;
 public interface CSSService {
     /**
      * 将less文件编译成css文件
-     * @param cssName 输出css文件路劲 全路劲
+     *
+     * @param cssName  输出css文件路劲 全路劲
      * @param fileName less文件 全路劲
      */
-    void convertLess2Css(String fileName,String cssName);
+    void convertLess2Css(String fileName, String cssName);
+
+    /**
+     * 将页面主题转换为既定规格的css
+     *
+     * @param theme        页面主题
+     * @param outputStream css的输入流
+     * @throws IOException              如果保存css发生错误
+     * @throws IllegalArgumentException 主题信息不符合要求
+     */
+    void convertCss(PageTheme theme, OutputStream outputStream) throws IOException, IllegalArgumentException;
 
 }

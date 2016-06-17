ca9e9cdcd366ecb73ef27ebfd948df91660ee815
HZCJ <luffy.ja@gmail.com>
2016-06-17 14:55:31 +0800

    增加安全相关的仓库

---
+++ service/src/main/java/com/huotu/hotcms/service/service/LoginService.java
@@ -0,0 +1,37 @@
+/*
+ * 版权所有:杭州火图科技有限公司
+ * 地址:浙江省杭州市滨江区西兴街道阡陌路智慧E谷B幢4楼
+ *
+ * (c) Copyright Hangzhou Hot Technology Co., Ltd.
+ * Floor 4,Block B,Wisdom E Valley,Qianmo Road,Binjiang District
+ * 2013-2016. All rights reserved.
+ */
+
+package com.huotu.hotcms.service.service;
+
+import com.huotu.hotcms.service.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+/**
+ * 登录服务
+ *
+ * @author CJ
+ */
+@Service
+public class LoginService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserDetails userDetails = userRepository.findByLoginName(username);
+        if (userDetails == null)
+            throw new UsernameNotFoundException("can not find " + username);
+        return userDetails;
+    }
+}

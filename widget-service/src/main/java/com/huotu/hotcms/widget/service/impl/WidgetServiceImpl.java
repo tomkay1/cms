293fe5e5a9c63f9f118ca31f695b867d7a7772f0
lihuaixin <1072931117@qq.com>
2016-06-21 13:51:38 +0800

    标签自定义模板解析

--- widget-service/src/main/java/com/huotu/hotcms/widget/service/impl/WidgetServiceImpl.java
+++ widget-service/src/main/java/com/huotu/hotcms/widget/service/impl/WidgetServiceImpl.java
@@ -10,31 +10,58 @@
 package com.huotu.hotcms.widget.service.impl;
 
 import com.huotu.hotcms.widget.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.io.Resource;
+import org.thymeleaf.context.WebEngineContext;
+import org.thymeleaf.spring4.SpringTemplateEngine;
 
+import java.io.IOException;
 import java.net.URI;
+import java.util.Collections;
+import java.util.Map;
 
 /**
  * Created by elvis on 2016/6/7.
  */
 public class WidgetServiceImpl implements WidgetService {
 
+    @Autowired
+    ApplicationContext applicationContext;
+
+
     @Override
     public URI resourceURI(Widget widget, String resourceName) {
+        Map<String ,Resource> publicResources = widget.publicResources();
+        Resource resource = publicResources.get(resourceName);
+        try {
+            return resource.getURI();
+        } catch (IOException e) {
+        }
         return null;
     }
 
     @Override
-    public String previewHTML(Widget widget, String styleId, CMSContext context, ComponentProperties properties) {
-        return null;
+    public String previewHTML(Widget widget, String styleId, CMSContext cmsContext, ComponentProperties properties, WebEngineContext context) {
+        SpringTemplateEngine engine = applicationContext.getBean(SpringTemplateEngine.class);
+        String finalHTML = engine.process("TEMPLATE/" +  ((Widget) context.getVariable("widget")).widgetId().replace('-', '.')
+                + "/" + styleId , Collections.singleton("div"), context);
+        return finalHTML;
     }
 
     @Override
-    public String editorHTML(Widget widget, CMSContext context) {
-        return null;
+    public String editorHTML(Widget widget, CMSContext cmsContext, WebEngineContext context) {
+        SpringTemplateEngine engine = applicationContext.getBean(SpringTemplateEngine.class);
+        String finalHTML = engine.process("EDITOR/"+ ((Widget) context.getVariable("widget")).widgetId().replace('-', '.')
+                , Collections.singleton("div"), context);
+        return finalHTML;
     }
 
     @Override
-    public String componentHTML(Component component, CMSContext context) {
+    public String componentHTML(Component component, CMSContext cmsContext, WebEngineContext context) {
+        Widget widget =  component.getWidget().getWidget();
+        String styleId = component.getStyleId();
+        ComponentProperties componentProperties = component.getProperties();
         return null;
     }
 }

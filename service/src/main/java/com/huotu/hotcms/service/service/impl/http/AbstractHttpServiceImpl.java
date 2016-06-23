26417d9a42deeca3e3a8c7ed6cfe18d27655f0ea
HZCJ <luffy.ja@gmail.com>
2016-06-23 10:58:06 +0800

    make runnable in simple server.

--- service/src/main/java/com/huotu/hotcms/service/service/impl/http/AbstractHttpServiceImpl.java
+++ service/src/main/java/com/huotu/hotcms/service/service/impl/http/AbstractHttpServiceImpl.java
@@ -1,3 +1,12 @@
+/*
+ * 版权所有:杭州火图科技有限公司
+ * 地址:浙江省杭州市滨江区西兴街道阡陌路智慧E谷B幢4楼
+ *
+ * (c) Copyright Hangzhou Hot Technology Co., Ltd.
+ * Floor 4,Block B,Wisdom E Valley,Qianmo Road,Binjiang District
+ * 2013-2016. All rights reserved.
+ */
+
 package com.huotu.hotcms.service.service.impl.http;
 
 import com.huotu.hotcms.service.service.HttpService;
@@ -27,10 +36,10 @@ import java.util.Map;
  * Created by Administrator on 2016/4/19.
  */
 public abstract class AbstractHttpServiceImpl implements HttpService {
+    String appRoot = "http://test.api.open.huobanplus.com:8081";
+    String appKey = "_demo";
+    String appSecret = "1f2f3f4f5f6f7f8f";
     private Log log = LogFactory.getLog(getClass());
-    protected String appRoot="http://test.api.open.huobanplus.com:8081";
-    protected String appKey="_demo";
-    protected String appSecret="1f2f3f4f5f6f7f8f";
 
 //    @Override
 //    public ApiResult<String> httpGet_prod(String scheme, String host, Integer port, String path, Map<String, Object> params) {

4d760f81a09e7890509d2723052825e64177587c
HZCJ <luffy.ja@gmail.com>
2016-07-06 09:45:22 +0800

    修改.

--- service/src/main/java/com/huotu/hotcms/service/entity/PageInfo.java
+++ service/src/main/java/com/huotu/hotcms/service/entity/PageInfo.java
@@ -1,6 +1,7 @@
 /*
  * 版权所有:杭州火图科技有限公司
  * 地址:浙江省杭州市滨江区西兴街道阡陌路智慧E谷B幢4楼
+ *
  * (c) Copyright Hangzhou Hot Technology Co., Ltd.
  * Floor 4,Block B,Wisdom E Valley,Qianmo Road,Binjiang District
  * 2013-2016. All rights reserved.
@@ -12,7 +13,15 @@ import com.huotu.hotcms.service.common.PageType;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import java.time.LocalDateTime;
 
 
@@ -30,15 +39,21 @@ public class PageInfo {
     @Column(name = "pageId")
     private Long pageId;
 
-    @Column(name = "pagePath", unique = true, length = 60)
+    @Column(name = "pagePath", length = 60)
     private String pagePath;
 
     @Column(name = "title", length = 60)
     private String title;
 
+    /**
+     * 可选的数据源
+     */
     @ManyToOne
     @JoinColumn(name = "categoryId")
     private Category category;
+
+    @ManyToOne(optional = false)
+    private Site site;
 
     @Column(name = "createTime")
     private LocalDateTime createTime;

1f8e96fd27aafeaefe6bba4b0dd7fb764358b925
HZCJ <luffy.ja@gmail.com>
2016-07-14 10:35:27 +0800

    应付Spring BUG.

---
+++ manage-service/src/main/java/com/huotu/cms/manage/MethodParameterFixed.java
@@ -0,0 +1,27 @@
+/*
+ * 版权所有:杭州火图科技有限公司
+ * 地址:浙江省杭州市滨江区西兴街道阡陌路智慧E谷B幢4楼
+ *
+ * (c) Copyright Hangzhou Hot Technology Co., Ltd.
+ * Floor 4,Block B,Wisdom E Valley,Qianmo Road,Binjiang District
+ * 2013-2016. All rights reserved.
+ */
+
+package com.huotu.cms.manage;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 为了修复Spring {@link org.springframework.core.MethodParameter#equals(Object)}的不足
+ *
+ * @author CJ
+ */
+@Target({ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface MethodParameterFixed {
+}

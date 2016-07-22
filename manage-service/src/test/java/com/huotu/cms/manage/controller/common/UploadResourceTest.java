b7fd33ecbbbfb9c5d4d4b3667464f0dfc95501b3
lihuaixin <1072931117@qq.com>
2016-07-22 16:14:40 +0800

    page 添加seo，标题...

---
+++ manage-service/src/test/java/com/huotu/cms/manage/controller/common/UploadResourceTest.java
@@ -0,0 +1,8 @@
+package com.huotu.cms.manage.controller.common;
+
+/**
+ * Created by lhx on 2016/7/21.
+ */
+
+public class UploadResourceTest {
+}
---
+++ service/src/main/java/com/huotu/hotcms/service/model/NavbarPageInfoModel.java
@@ -0,0 +1,8 @@
+package com.huotu.hotcms.service.model;
+
+/**
+ * Created by lhx on 2016/7/21.
+ */
+
+public class NavbarPageInfoModel {
+}

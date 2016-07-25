dd8042a05243bf6b441994c4a5684bf26968cdec
HZCJ <luffy.ja@gmail.com>
2016-07-25 17:27:41 +0800

    rewrite for better readable.

--- manage-service/src/test/java/com/huotu/cms/manage/controller/common/UploadResourceTest.java
+++ manage-service/src/test/java/com/huotu/cms/manage/controller/common/UploadResourceTest.java
@@ -1,3 +1,12 @@
+/*
+ * 版权所有:杭州火图科技有限公司
+ * 地址:浙江省杭州市滨江区西兴街道阡陌路智慧E谷B幢4楼
+ *
+ * (c) Copyright Hangzhou Hot Technology Co., Ltd.
+ * Floor 4,Block B,Wisdom E Valley,Qianmo Road,Binjiang District
+ * 2013-2016. All rights reserved.
+ */
+
 package com.huotu.cms.manage.controller.common;
 
 import com.huotu.cms.manage.ManageTest;
@@ -6,14 +15,9 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.mock.web.MockMultipartFile;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-
-/**
- * Created by lhx on 2016/7/21.
- */
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class UploadResourceTest extends ManageTest {
 
@@ -21,17 +25,18 @@ public class UploadResourceTest extends ManageTest {
     public void uploadResource() throws Exception {
         loginAsOwner(testOwner);
         Resource resource = new ClassPathResource("thumbnail.png");
-        int code = mockMvc.perform(post("/manage/cms/resourceUpload", new MockMultipartFile("file", resource.getInputStream()))
-        ).andDo(print()).andReturn().getResponse().getStatus();
-        assertThat(code).isEqualTo(200);
+
+        mockMvc.perform(
+                post("/manage/cms/resourceUpload"
+                        , new MockMultipartFile("file", resource.getInputStream())).session(session)
+        ).andExpect(status().isOk());
     }
 
     @Test
     public void deleteResource() throws Exception {
         loginAsOwner(testOwner);
-        int code = mockMvc.perform(delete("/manage/cms/deleteResource", "thumbnail.png")
-        ).andDo(print()).andReturn().getResponse().getStatus();
-        assertThat(code).isEqualTo(200);
+        mockMvc.perform(delete("/manage/cms/deleteResource", "thumbnail.png").session(session))
+                .andExpect(status().isOk());
     }
 
 }

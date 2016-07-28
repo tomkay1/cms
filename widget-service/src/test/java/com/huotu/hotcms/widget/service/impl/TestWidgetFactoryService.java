a478155c0a77fcc90ce8d6ddf0c7e1912a1e59fa
HZCJ <luffy.ja@gmail.com>
2016-07-28 15:05:00 +0800

    add Transactional

--- widget-service/src/test/java/com/huotu/hotcms/widget/service/impl/TestWidgetFactoryService.java
+++ widget-service/src/test/java/com/huotu/hotcms/widget/service/impl/TestWidgetFactoryService.java
@@ -43,7 +43,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -67,7 +66,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 @Import(ServiceConfig.class)
 @WebAppConfiguration
 @Transactional
-@Rollback()
 public class TestWidgetFactoryService extends TestBase {
 
     @Autowired(required = false)

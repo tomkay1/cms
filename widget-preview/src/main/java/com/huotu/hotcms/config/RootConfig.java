0eb03f8afd7327d47c7b29f544dde5f2df372c47
lihuaixin <1072931117@qq.com>
2016-08-03 16:42:13 +0800

    添加preview html实现相关实现

--- widget-preview/src/main/java/com/huotu/hotcms/config/RootConfig.java
+++ widget-preview/src/main/java/com/huotu/hotcms/config/RootConfig.java
@@ -2,14 +2,13 @@ package com.huotu.hotcms.config;
 
 import com.huotu.hotcms.service.config.ServiceConfig;
 import com.huotu.hotcms.service.entity.support.WidgetIdentifier;
-import com.huotu.hotcms.widget.WidgetLoaderConfig;
 import com.huotu.hotcms.widget.WidgetResolveServiceConfig;
 import com.huotu.hotcms.widget.controller.CMSDataSourceController;
 import com.huotu.hotcms.widget.controller.WidgetController;
 import com.huotu.hotcms.widget.service.CMSDataSourceService;
+import com.huotu.widget.test.WidgetTestConfig;
 import com.huotu.widget.test.bean.CMSDataSourceServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
@@ -19,20 +18,13 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.thymeleaf.dialect.IDialect;
-import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
-import org.thymeleaf.spring4.SpringTemplateEngine;
-import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring4.view.ThymeleafViewResolver;
-import org.thymeleaf.templatemode.TemplateMode;
-
-import java.util.Set;
 
 /**
  * Created by lhx on 2016/8/3.
  */
 @EnableWebMvc
-@Import({RootConfig.ViewResolver.class, WidgetResolveServiceConfig.class, ServiceConfig.class})
+@Import({WidgetTestConfig.ViewResolver.class, WidgetResolveServiceConfig.class, ServiceConfig.class})
 @ComponentScan(value = {"com.huotu.hotcms.bean"})
 public class RootConfig extends WebMvcConfigurerAdapter {
 
@@ -82,61 +74,5 @@ public class RootConfig extends WebMvcConfigurerAdapter {
         return new CMSDataSourceServiceImpl();
     }
 
-    @Import(WidgetLoaderConfig.class)
-    static class ViewResolver {
 
-        @Autowired
-        private ApplicationContext applicationContext;
-
-        @Autowired
-        private Set<IDialect> dialectSet;
-
-        @Bean
-        public ThymeleafViewResolver normalViewResolver() {
-            SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
-            templateResolver.setCharacterEncoding("UTF-8");
-            templateResolver.setApplicationContext(applicationContext);
-            templateResolver.setPrefix("classpath:/testPages/");
-            templateResolver.setSuffix(".html");
-
-//            widgetTemplateResolver.setOrder(1);
-
-            SpringTemplateEngine engine = new SpringTemplateEngine();
-            engine.addTemplateResolver(templateResolver);
-//            engine.addTemplateResolver(widgetTemplateResolver);
-            engine.setAdditionalDialects(dialectSet);
-
-            ThymeleafViewResolver resolver = new ThymeleafViewResolver();
-            resolver.setCharacterEncoding("UTF-8");
-            resolver.setContentType("text/html;charset=UTF-8");
-            resolver.setTemplateEngine(engine);
-            resolver.setOrder(3);
-            return resolver;
-        }
-
-        @Bean
-        public ThymeleafViewResolver javascriptThymeleafViewResolver() {
-
-            SpringResourceTemplateResolver rootTemplateResolver =
-                    new SpringResourceTemplateResolver();
-            rootTemplateResolver.setCheckExistence(true);
-            rootTemplateResolver.setPrefix("/_resources/testWidget/js/");
-            rootTemplateResolver.setApplicationContext(applicationContext);
-            rootTemplateResolver.setTemplateMode(TemplateMode.JAVASCRIPT);
-            rootTemplateResolver.setCharacterEncoding("UTF-8");
-
-            SpringTemplateEngine engine = new SpringTemplateEngine();
-            engine.addDialect(new SpringSecurityDialect());
-            engine.setTemplateResolver(rootTemplateResolver);
-
-            ThymeleafViewResolver resolver = new ThymeleafViewResolver();
-            resolver.setOrder(2);
-            resolver.setCharacterEncoding("UTF-8");
-            resolver.setTemplateEngine(engine);
-            resolver.setApplicationContext(applicationContext);
-            resolver.setViewNames(new String[]{"*.js", "*.JS"});
-            resolver.setContentType("application/javascript");
-            return resolver;
-        }
-    }
 }

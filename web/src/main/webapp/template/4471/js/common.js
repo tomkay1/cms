ab2bcd3e9df0e676e74e5b66dc216d530a4d1290
HZCJ <luffy.ja@gmail.com>
2016-08-03 15:50:31 +0800

    remove all them!!

--- web/src/main/webapp/template/4471/js/common.js
+++
@@ -1,8 +0,0 @@
-var navigate= {
-    topCategoryArtive: function (index, div, element) {
-        $($("#" + div).find(element).get(index)).addClass("active");
-    },
-    CategoryActive:function(index, div, element,active){
-        $($("#" + div).find(element).get(index)).addClass(active);
-    }
-}

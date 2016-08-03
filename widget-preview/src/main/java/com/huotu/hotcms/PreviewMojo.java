e747491ddb550b213d89559e47b4649ea5e5c69d
HZCJ <luffy.ja@gmail.com>
2016-08-03 20:53:36 +0800

    enhance

--- widget-preview/src/main/java/com/huotu/hotcms/PreviewMojo.java
+++ widget-preview/src/main/java/com/huotu/hotcms/PreviewMojo.java
@@ -1,3 +1,12 @@
+/*
+ * 版权所有:杭州火图科技有限公司
+ * 地址:浙江省杭州市滨江区西兴街道阡陌路智慧E谷B幢4楼
+ *
+ * (c) Copyright Hangzhou Hot Technology Co., Ltd.
+ * Floor 4,Block B,Wisdom E Valley,Qianmo Road,Binjiang District
+ * 2013-2016. All rights reserved.
+ */
+
 package com.huotu.hotcms;
 
 import org.apache.maven.plugin.AbstractMojo;
@@ -20,28 +29,28 @@ public class PreviewMojo extends AbstractMojo {
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
         try {
-            //robot 用来做什么我还不清楚，
-            Robot robot = new Robot();
-            InetAddress inetAddress = InetAddress.getByName("127.0.0.1");
-            Integer port = null;
+            InetAddress inetAddress = InetAddress.getLocalHost();
+            int port = 0;
             for (int i = 10000; i < 65535; i++) {
                 try {
-                    Socket socket = new Socket(inetAddress, i);
+                    new Socket(inetAddress, i);
                     port = i;
-                } catch (IOException e) {
+                } catch (IOException ignored) {
                     //端口连接失败
-                    getLog().info(i + "端口占用");
+//                    getLog().info(i + "端口占用");
                 }
             }
-            EmbeddedTomcat tomcat = new EmbeddedTomcat(port.intValue());
+            getLog().debug("Preview Http Server will use port " + port);
+            EmbeddedTomcat tomcat = new EmbeddedTomcat(port);
             tomcat.start();
             Desktop desktop = Desktop.getDesktop();
+
+
             //todo 打开控件浏览视图或者编辑视图通过widgetName进行打开
             desktop.browse(new URI("http://localhost:" + port + "/"));
 
         } catch (Exception e) {
             getLog().error("preview", e);
         }
-        getLog().info("preview");
     }
 }

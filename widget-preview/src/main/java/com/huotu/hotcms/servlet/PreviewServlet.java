e35da243a68ac7a147d70dbf816e32d81dda41c0
lihuaixin <1072931117@qq.com>
2016-08-03 13:38:57 +0800

    添加widget-preview相关实现

--- widget-preview/src/main/java/com/huotu/hotcms/servlet/PreviewServlet.java
+++ widget-preview/src/main/java/com/huotu/hotcms/servlet/PreviewServlet.java
@@ -9,16 +9,20 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet(
-        name = "MyServlet",
+        name = "PreviewServlet",
         urlPatterns = {"/preview"}
 )
-public class IndexServlet extends HttpServlet {
+public class PreviewServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         ServletOutputStream out = resp.getOutputStream();
-        out.write("hello world".getBytes());
+        if (req.getParameter("browse") != null) {
+            out.write("widget browse".getBytes());
+        } else if (req.getParameter("editor") != null) {
+            out.write("widget editor".getBytes());
+        }
         out.flush();
         out.close();
     }

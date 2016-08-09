3749133ed0811def01d16f7b429401a97f00a360
HZCJ <luffy.ja@gmail.com>
2016-08-09 11:18:19 +0800

    #7 增加富文本处理

--- service/src/main/java/com/huotu/hotcms/service/common/ArticleSource.java
+++ service/src/main/java/com/huotu/hotcms/service/common/ArticleSource.java
@@ -2,21 +2,23 @@
  * 版权所有:杭州火图科技有限公司
  * 地址:浙江省杭州市滨江区西兴街道阡陌路智慧E谷B幢4楼
  *
- *  (c) Copyright Hangzhou Hot Technology Co., Ltd.
- *  Floor 4,Block B,Wisdom E Valley,Qianmo Road,Binjiang District 2013-2015. All rights reserved.
+ * (c) Copyright Hangzhou Hot Technology Co., Ltd.
+ * Floor 4,Block B,Wisdom E Valley,Qianmo Road,Binjiang District
+ * 2013-2016. All rights reserved.
  */
 
 package com.huotu.hotcms.service.common;
 
 /**
- * Created by Administrator on 2015/12/21.
+ * 文章来源
  */
 public enum ArticleSource implements CommonEnum {
-    REPRINT(0,"转载"),
-    ORIGINAL(1,"原创");
+    REPRINT(0, "转载"),
+    ORIGINAL(1, "原创");
 
     private int code;
     private String value;
+
     ArticleSource(int code, String value) {
         this.code = code;
         this.value = value;

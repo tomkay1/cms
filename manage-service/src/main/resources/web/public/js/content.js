dddfe99fa57d8d5e689dac26c3263d52d9cf4170
HZCJ <luffy.ja@gmail.com>
2016-08-22 16:24:19 +0800

    使用h5 datalist 不支持也就不支持了……

--- manage-service/src/main/resources/web/public/js/content.js
+++ manage-service/src/main/resources/web/public/js/content.js
@@ -19,5 +19,41 @@ $(function () {
     $.cmsUploader($('#download-uploader'), function (path) {
         $("#downloadUrl").val(path);
     });
+
+    var categories = $('option', $('#categories'));
+
+    var parentCategoryId = $('select[name=parentCategoryId]');
+
+    var categoryName = $('input[name=categoryName]');
+    categoryName.change(function () {
+
+        // 找到那个option
+        var val = $(this).val();
+        // 我们找下是否在列表内
+
+        var targetOption = categories.filter(function (index, ele) {
+            return $(ele).val() == val;
+        }).first();
+
+        if (targetOption.size() > 0) {
+            // 的确是选择的
+            parentCategoryId.prop('disabled', 'disabled');
+            var parentId = targetOption.attr('parentId');
+            if (parentId && parentId.length > 0) {
+                //有父类的
+                parentCategoryId.val(parentId);
+            } else {
+                //置空
+                parentCategoryId.val('');
+            }
+        } else {
+            // 手工录入的 保留原值么?
+            parentCategoryId.prop('disabled', '');
+        }
+
+        console.log('changed', val, categories, targetOption);
+
+    });
+
 });
 
